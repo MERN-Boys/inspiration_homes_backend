587f1f911669a5a2ffe5c9dc903ebcce9529c643
BenW <bwakefield34@gmail.com>
2021-01-28 14:32:50 +1000

    CORS Fix #2

--- index.js
+++ index.js
@@ -26,22 +26,22 @@ mongoose.connect(connectionString, {
 .then(() => console.log("Connected to the database"))
 .catch(error => console.log(error))
 
-
-
-//Middleware
-app.use((req, res, next) => {
+var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
-        res.send(200);
+      res.send(200);
     }
     else {
-        next();
+      next();
     }
-})
+};
+
+//Middleware
+app.use(allowCrossDomain)
 app.use(express.json())
 
 app.use("/jobs", require("./routes/jobs.js"))
