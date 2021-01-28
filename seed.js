907136443e5b25e7476f347e02e8adbe1d497de4
BenW <bwakefield34@gmail.com>
2021-01-28 11:57:43 +1000

    Created file that seeds the database with a Builder, Client, and a Job connected to the Client

--- seed.js
+++ seed.js
@@ -1,19 +1,67 @@
-// const UserModel = require("./models/user.js")
-// const JobModel = require("./models/jobs.js")
+const UserModel = require("./models/users.js")
+const JobModel = require("./models/jobs.js")
 
-// UserModel.create({
-//     "name": "TestClient"
-// })
-// .then((document) => console.log(document))
-// .catch((error) => console.log(error)) 
+const mongoose = require("mongoose")
+const { config } = require('dotenv');
 
-// import seeder from "mongoose-seed"
+config();
+const username = process.env.MONGO_USERNAME  
+const password = process.env.MONGO_PASSWORD
+const database = process.env.MONGO_DBNAME  
 
-// const connectionString = `mongodb+srv://${username}:${password}@cluster0.rqngg.mongodb.net/${database}?retryWrites=true&w=majority`
+const connectionString = `mongodb+srv://${username}:${password}@cluster0.rqngg.mongodb.net/${database}?retryWrites=true&w=majority`
 
-// mongoose.connect(connectionString, {
-//     seeder.loadModels()
-// })
-// .then(() => console.log("Connected to the database"))
-// .catch(error => console.log(error))
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Connected to the database"))
+.catch(error => console.log(error))
+
+const users = [
+    new UserModel({
+        "name": "TestClient",
+        "email": "example@google.com",
+        "password": "password"
+    }),
+    new UserModel({
+        "name": "Builder",
+        "email": "example@google.com",
+        "password": "password"
+    })
+]
+
+const jobs = [
+    new JobModel({
+        "client": "0",
+        "jobTitle": "New Job",
+        "buildAddress": "6 Langdon Lane Bellmere 4510",
+        "designDocs": {
+            "link": "examplelink.com",
+            "description": "Great balls of fire"
+        }
+    })
+]
+UserModel.deleteMany({})
+.then(() => JobModel.deleteMany({}))
+.then(() => {
+    users.map(async(u, index) => {
+        u.save((err, result) => {
+            if (index === users.length - 1){
+                console.log("SEEDED USERS")
+                UserModel.findOne({"name": "TestClient"})
+                .then((user) => {
+                    jobs[0].client = user._id
+                    return jobs[0]
+                })
+                .then(job => {
+                    job.save((err, result) => {
+                        console.log("SEEDED JOB")
+                        mongoose.disconnect();
+                    })
+                })
+            }
+        })
+    })
+})
 
