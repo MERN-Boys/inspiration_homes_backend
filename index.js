cebd16c88c039b031259a17a7a36be6a6af6edd4
BenW <bwakefield34@gmail.com>
2021-01-28 14:29:06 +1000

    Fixing CORS

--- index.js
+++ index.js
@@ -29,6 +29,19 @@ mongoose.connect(connectionString, {
 
 
 //Middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+})
 app.use(express.json())
 
 app.use("/jobs", require("./routes/jobs.js"))
