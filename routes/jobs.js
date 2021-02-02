68113db1d4d98109d4c6f1b67f29f30ea980a206
BenW <bwakefield34@gmail.com>
2021-02-02 17:22:24 +1000

    Cleaned up jobs routes page

--- routes/jobs.js
+++ routes/jobs.js
@@ -11,50 +11,6 @@ const ID = process.env.AWS_ID
 const SECRET = process.env.AWS_SECRET
 const BUCKET_NAME = process.env.AWS_BUCKET
 
-// const uploadFileNew = (arrOfFiles) => {
-//     // Read content from the file
-//     // const fileContent = fs.readFileSync(fileName);
-
-//     var promises=[];
-
-//     for(var i=0; i<arrOfFiles.length; i++){
-//         var file = arrOfFiles[i];
-//         promises.push(s3Upload(file));
-//     }
-//     Promise.all(promises).then(function(data){
-//         // res.send(data);
-//         console.log("Promises")
-//     }).catch(function(err){
-//         // res.send(err.stack);
-//         console.log(err)
-//     }) 
-//     // Setting up S3 upload parameters
-    
-// }
-
-// const s3Upload = (obj) => {
-//     // console.log(obj)
-//     const s3 = new AWS.S3({
-//         accessKeyId: ID,
-//         secretAccessKey: SECRET
-//     })
-
-//     const params = {
-//         Bucket: BUCKET_NAME,
-//         Key: obj.filename, // File name you want to save as in S3
-//         Body: obj.content
-//     };
-
-//     // Uploading files to the bucket
-//     return s3.upload(params, (err, data) => {
-//         if (err) {
-//             throw err;
-//         }
-//         console.log(`File uploaded successfully.`);
-//         console.log(data.Location);
-//     })
-// }
-
 /*
 {
     "name": "Benjamin",
